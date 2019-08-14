d3bcafe6eee52c4a18591ab15dfe5c45501410a4
xxcodingmasterxx <53945586+xxcodingmasterxx@users.noreply.github.com>
2019-08-14 10:54:55 -0400

    Create divide_by_2.py

---
+++ divide_by_2.py
@@ -0,0 +1,36 @@
+def divide_by_two(x, y):
+  result = x / y
+  print(result)
+  
+
+
+divide_by_two(4, 2)
+
+
+"""
+always start the definiton of a function with the keyword "def"
+next, write a good name for the function, which is "function_name" in this case
+always have parentheses and your parameters inside them
+conclude the first line with a colon
+we indent on the next line and every line after if we are still inside 
+the function
+
+
+def function_name(parameter1, parameter2, ...):
+  print(parameter1)
+  print(parameter2)
+  print(...)
+  print(parameter1 + parameter2)
+
+function_name(5, 10)
+"""
+
+"""
+def function_name(student, teacher, person):
+  print(teacher)
+  print(person)
+  print(student)
+
+function_name("barron", "ethan", "john")
+function_name(5, 2, 1)
+"""
